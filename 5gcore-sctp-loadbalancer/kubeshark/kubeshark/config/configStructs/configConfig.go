c5fe99efcdc1a2c2d522294135fa02358547f2ce
vrajpat3ll <vrajpat3l264@gmail.com>
2025-11-23 01:48:37 +0530

    add: submodule for kubeshark

--- 5gcore-sctp-loadbalancer/kubeshark/kubeshark/config/configStructs/configConfig.go
+++
@@ -1,9 +0,0 @@
-package configStructs
-
-const (
-	RegenerateConfigName = "regenerate"
-)
-
-type ConfigConfig struct {
-	Regenerate bool `yaml:"regenerate,omitempty" json:"regenerate,omitempty" default:"false" readonly:""`
-}
