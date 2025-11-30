87f13abf768cd5347d6b3f7a07cd24e5ba322cd6
vrajpat3ll <vrajpat3l264@gmail.com>
2025-11-30 15:14:31 +0530

    fix stop script

--- tests/stop_ue.py
+++ tests/stop_ue.py
@@ -6,7 +6,7 @@ Uses the same trace file and durations to schedule shutdowns.
 Generates the events.json output.
 
 Example command:
-python3 stop_ue.py --trace traces/400ue-run.json --log-dir ue_logs --events events/400ue_events.json
+python3 stop_ue.py --trace traces/400ue-run.json --log-dir ue_logs --num_ues 100 --events events/400ue_events.json
 """
 
 import argparse
@@ -65,36 +65,39 @@ def stop_single_ue(ue, start_time, log_dir, use_sudo):
 
     log_info(f"UE {ue_id} | Attempting graceful shutdown")
 
-    event["actual_start_time"] = iso_now()
+    # event["actual_stop_time"] = iso_now()
 
-    # graceful
-    try:
-        sig_cmd = [
-            "kubectl", "-n", ns, "exec", "deploy/sim5g-simulator", "--",
-            "bash", "-c", "pkill -SIGTERM app || true",
-        ]
-        if use_sudo:
-            sig_cmd.insert(0, "sudo")
-        subprocess.run(sig_cmd, timeout=10)
-        event["termination_method"] = "graceful"
-    except subprocess.TimeoutExpired:
-        log_warn(f"UE {ue_id} | SIGTERM timeout")
-        event["termination_method"] = "graceful-timeout"
+    # # graceful
+    # try:
+    #     sig_cmd = [
+    #         "kubectl", "-n", ns, "exec", "deploy/sim5g-simulator", "--",
+    #         "bash", "-c", "pkill -SIGTERM app || true",
+    #     ]
+    #     if use_sudo:
+    #         sig_cmd.insert(0, "sudo")
+    #     subprocess.run(sig_cmd, timeout=10)
+    #     event["termination_method"] = "graceful"
+    # except subprocess.TimeoutExpired:
+    #     log_warn(f"UE {ue_id} | SIGTERM timeout")
+    #     event["termination_method"] = "graceful-timeout"
 
-    time.sleep(3)
+    # time.sleep(3)
 
     # force delete deployment for safety
-    del_cmd = [
-        "kubectl", "-n", ns, "delete",
-        "deploy/sim5g-simulator", "--ignore-not-found=true",
-    ]
-    if use_sudo:
-        del_cmd.insert(0, "sudo")
+    try:
+        del_cmd = [
+            "kubectl", "-n", ns, "delete",
+            "deploy/sim5g-simulator", "--ignore-not-found=true",
+        ]
+        if use_sudo:
+            del_cmd.insert(0, "sudo")
 
-    subprocess.run(del_cmd)
-
-    event["actual_stop_time"] = iso_now()
-    event["exit_code"] = 0
+        output = subprocess.run(del_cmd)
+        # output.check_returncode()
+        event["actual_stop_time"] = iso_now()
+        event["exit_code"] = output.returncode
+    except Exception as e:
+        log_warn(f"UE{ue_id} got issue: {e}")
 
     with lock:
         events.append(event)
@@ -108,6 +111,7 @@ def main():
     p.add_argument("--log-dir", default="ue_logs")
     p.add_argument("--events", default="events.json")
     p.add_argument("--no-sudo", action="store_true")
+    p.add_argument("--num_ues", deafult=100)
     args = p.parse_args()
 
     trace_path = Path(args.trace)
@@ -128,8 +132,12 @@ def main():
 
     start_time = time.time()
     threads = []
-
+    i = 0
     for ue in ues:
+        if i < args.num_ues:
+            i+=1
+        else: 
+            break
         t = threading.Thread(
             target=stop_single_ue, 
             args=(ue, start_time, args.log_dir, use_sudo)
